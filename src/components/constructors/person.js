f49b6caa460da402595d14be1e8f62fad1290b14
martin-banks <anotherbanksy@gmail.com>
2020-03-30 18:31:22 +1100

    adds new branch to store gatsby integration before changeing to vanilla

--- src/components/constructors/person.js
+++ src/components/constructors/person.js
@@ -1,5 +1,5 @@
 class Person {
-  constructor ({ P5, x, y, index, diameter, infected, status, moving, people}) {
+  constructor ({ x, y, index, diameter, infected, status, moving, speed, width, height }) {
     this.infected = infected
     this.status = status
     this.infectedTime = infected ? new Date().getTime() : null
@@ -8,24 +8,28 @@ class Person {
     this.diameter = diameter
     this.position = { x, y }
     this.velocity = {
-      x: P5.random(-1, 1) * speed,
-      y: P5.random(-1, 1) * speed,
+      // x: P5.random(-1, 1) * speed,
+      x: ((Math.random() - 0.5) * 2) * (speed),
+      // y: P5.random(-1, 1) * speed,
+      y: ((Math.random() - 0.5) * 2) * (speed),
     }
     this.order = 1
+    this.minDist = diameter / 2
+    this.canvas = { width, height }
   }
 
   move () {
     this.position.x += this.velocity.x
     this.position.y += this.velocity.y
-    if ((this.position.x + (this.diameter / 2)) > width) {
-      this.position.x = width - (this.diameter / 2)
+    if ((this.position.x + (this.diameter / 2)) > this.canvas.width) {
+      this.position.x = this.canvas.width - (this.diameter / 2)
       this.velocity.x *= -1
     } else if ((this.position.x - (this.diameter / 2)) < 0) {
       this.position.x = this.diameter / 2
       this.velocity.x *= -1
     }
-    if ((this.position.y + (this.diameter / 2)) > height) {
-      this.position.y = height - (this.diameter / 2)
+    if ((this.position.y + (this.diameter / 2)) > this.canvas.height) {
+      this.position.y = this.canvas.height - (this.diameter / 2)
       this.velocity.y *= -1
     } else if ((this.position.y - (this.diameter / 2)) < 0) {
       this.position.y = this.diameter / 2
@@ -33,92 +37,96 @@ class Person {
     }
   }
 
-  intersect () {
-    if (this.status === 'dead') return
-    for (let i = this.id + 1; i < population; i++) {
-      if (this.id === i) return
-      if (people[i].status === 'dead') continue
+  // intersect (people) {
+  //   // ? people is an array of Person objects craeted by this constructor
+  //   if (!people) return console.error('people array is missing')
+  //   if (this.status === 'dead') return
+  //   for (let i = this.id + 1; i < population; i++) {
+  //     if (this.id === i) return
+  //     if (people[i].status === 'dead') continue
 
-      let distanceX = people[i].position.x - this.position.x
-      let distanceY = people[i].position.y - this.position.y
-      let distance = P5.sqrt((distanceX * distanceX) + (distanceY * distanceY))
-      let minDist = (people[i].diameter / 2) + (this.diameter / 2)
+  //     let distanceX = people[i].position.x - this.position.x
+  //     let distanceY = people[i].position.y - this.position.y
+  //     // let distance = this.P5.sqrt((distanceX * distanceX) + (distanceY * distanceY))
+  //     let distance = Math.sqrt((distanceX * distanceX) + (distanceY * distanceY))
+  //     // let minDist = (people[i].diameter / 2) + (this.diameter / 2)
 
-      if (distance < minDist) {
-        // Handle update directions
-        if (this.velocity.x > 0) {
-          if (this.position.x > people[i].position.x) {
-            people[i].velocity.x *= -1
-          } else {
-            this.velocity.x *= -1
-          }
-        } else {
-          if (this.position.x > people[i].position.x) {
-            this.velocity.x *= -1
-          } else {
-            people[i].velocity.x *= -1
-          }
-        }
+  //     if (distance < this.minDist) {
+  //       // Handle update directions
+  //       if (this.velocity.x > 0) {
+  //         if (this.position.x > people[i].position.x) {
+  //           people[i].velocity.x *= -1
+  //         } else {
+  //           this.velocity.x *= -1
+  //         }
+  //       } else {
+  //         if (this.position.x > people[i].position.x) {
+  //           this.velocity.x *= -1
+  //         } else {
+  //           people[i].velocity.x *= -1
+  //         }
+  //       }
 
-        if (this.velocity.y > 0) {
-          if (this.position.y > people[i].position.y) {
-            people[i].velocity.y *= -1
-          } else {
-            this.velocity.y *= -1
-          }
-        } else {
-          if (this.position.y > people[i].position.y) {
-            this.velocity.y *= -1
-          } else {
-            people[i].velocity.y *= -1
-          }
-        }
+  //       if (this.velocity.y > 0) {
+  //         if (this.position.y > people[i].position.y) {
+  //           people[i].velocity.y *= -1
+  //         } else {
+  //           this.velocity.y *= -1
+  //         }
+  //       } else {
+  //         if (this.position.y > people[i].position.y) {
+  //           this.velocity.y *= -1
+  //         } else {
+  //           people[i].velocity.y *= -1
+  //         }
+  //       }
 
-        // Handle update infected
-        if (!this.infected && !people[i].infected) {
-          return
-        } else if (this.infected && people[i].infected) {
-          return
+  //       // Handle update infected
+  //       if (!this.infected && !people[i].infected) {
+  //         return
+  //       } else if (this.infected && people[i].infected) {
+  //         return
 
-        } else if (this.status === 'normal' && people[i].status === 'infected') {
-          this.infected = true
-          this.status = 'infected'
-          this.order = 3
-          this.infectedTime = new Date().getTime()
-          totals.normal--
-          totals.infected++
-          return
+  //       } else if (this.status === 'normal' && people[i].status === 'infected') {
+  //         this.infected = true
+  //         this.status = 'infected'
+  //         this.order = 3
+  //         this.infectedTime = new Date().getTime()
+  //         totals.normal--
+  //         totals.infected++
+  //         return
 
-        } else if (this.status === 'infected' && people[i].status === 'normal') {
-          people[i].infected = true
-          people[i].status = 'infected'
-          people[i].order = 3
-          people[i].infectedTime = new Date().getTime()
-          totals.normal--
-          totals.infected++
-          return
-        }
-      }
-    }
-  }
+  //       } else if (this.status === 'infected' && people[i].status === 'normal') {
+  //         people[i].infected = true
+  //         people[i].status = 'infected'
+  //         people[i].order = 3
+  //         people[i].infectedTime = new Date().getTime()
+  //         totals.normal--
+  //         totals.infected++
+  //         return
+  //       }
+  //     }
+  //   }
+  // }
 
-  statusUpdate () {
-    if (this.status === 'normal') return
-    if (this.status === 'infected') {
-      const newStatus = random() < mortality ? 'dead' : 'cured'
-      if (this.infectedTime && ((this.infectedTime + timeToCure) <= new Date().getTime())) {
-        this.status = newStatus
-        this.order = newStatus === 'dead' ? 0
-          : newStatus === 'cured' ? 2
-            : 1
-        this.infected = false
-        totals.infected--
-        totals[newStatus]++
-      }
-    }
-  }
+  // statusUpdate () {
+  //   if (this.status === 'normal') return
+  //   if (this.status === 'infected') {
+  //     const newStatus = random() < mortality ? 'dead' : 'cured'
+  //     if (this.infectedTime && ((this.infectedTime + timeToCure) <= new Date().getTime())) {
+  //       this.status = newStatus
+  //       this.order = newStatus === 'dead' ? 0
+  //         : newStatus === 'cured' ? 2
+  //           : 1
+  //       this.infected = false
+  //       totals.infected--
+  //       totals[newStatus]++
+  //     }
+  //   }
+  // }
 
-  display (person) {
+  display (P5) {
+    console.log(this.id, P5)
     if (this.status === 'infected') {
       P5.fill(255, 0, 0)
     } else if (this.status === 'cured') {
@@ -132,3 +140,5 @@ class Person {
     P5.noStroke()
   }
 }
+
+export default Person
--- src/components/simulation-p5.js
+++ src/components/simulation-p5.js
@@ -4,34 +4,55 @@ import React, {
   useMutationEffect,
   useLayoutEffect
 } from 'react'
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"
 import PropTypes from "prop-types"
 import Styled from 'styled-components'
-import Sketch from 'react-p5'
+import p5 from 'p5'
+// import Sketch from 'react-p5'
 
 import Person from './constructors/person'
 
+// ? Create new P5 Sketch instance
+function Sketch (p) {
+  // p.setup = () => {
+    // p.noCanvas()
+    // p.createCanvas(700, 410)
+    //   .parent('parent')
+  // }
+
+  // p.draw = () => {
+  //   p.background(0)
+  //   p.fill(255)
+  //   p.rect(x, y, 50, 50)
+  // }
+}
+
+
+
 class Simulation extends React.Component {
   y = 0
-  resizeTracker = null
+  sketch = null
+  // resizeTracker = null
   constructor (props) {
     super()
+
     this.state = {
+      // sketch: null,
       // ? Canvas size
       width: 0,
       height: 0,
-      // canvas = { // TODO Set from designated area
-      //   w: Math.min(1000, window.innerWidth) - 60, // 400,
-      //   h: Math.min(1000, window.innerWidth) - 60, // 400,
-      // },
+      // // canvas = { // TODO Set from designated area
+      // //   w: Math.min(1000, window.innerWidth) - 60, // 400,
+      // //   h: Math.min(1000, window.innerWidth) - 60, // 400,
+      // // },
 
       // ? Config settings - move to props
-      population: 500,
+      // // population: 500,
       distancing: 0, // (pct of population that does not move)
       timeToCure: 8 * 1000, // (ms)
       mortality: 0.2, // (pct)
 
-      speed: 2, // (ms) how fast
+      // speed: 2, // (ms) how fast
       simulationLength: 30 * 1000, // (ms)
       sessionTick: 500, // How often does the chart update (ms)
 
@@ -54,34 +75,93 @@ class Simulation extends React.Component {
     // ? Bound Methods
     this.p5Setup = this.p5Setup.bind(this)
     this.storeParentSize = this.storeParentSize.bind(this)
+    this.createPeople = this.createPeople.bind(this)
   }
 
   // ? Methods
   storeParentSize () {
     this.setState(state => {
       const { offsetWidth, offsetHeight } = this.childRef.current.parentNode
+      console.log({ offsetWidth, offsetHeight })
       return {
-        width: offsetWidth,
-        height: offsetHeight,
+        width: parseInt(offsetWidth, 10),
+        height: parseInt(offsetHeight, 10),
         diameter: Math.round(Math.max(offsetWidth, offsetHeight) / 100),
       }
     })
   }
 
-  p5Setup ({ p5, parentRef }) {
-    p5.createCanvas(this.state.width, this.state.height)
-      .parent(parentRef)
+  p5Setup () {
+    // p5.createCanvas(this.state.width, this.state.height)
+    //   .parent(parentRef)
+    this.sketch.createCanvas(this.state.width, this.state.height)
+      .parent(this.childRef.current)
+  }
+
+  createPeople () {
+    return new Promise((resolve, reject) => {
+      const newPeople = []
+      for (let i = 0; i < parseInt(this.props.population, 10); i++) {
+        newPeople.push(
+          new Person({
+            x: Math.round(this.sketch.random(0, this.state.width)),
+            y: Math.round(this.sketch.random(0, this.state.height)),
+            width: this.state.width,
+            height: this.state.height,
+            index: i,
+            diameter: this.props.diameter,
+            infected: i < 1,
+            status: i < 1 ? 'infected' : 'normal',
+            moving: true,
+            speed: this.props.speed,
+          })
+        )
+      }
+      console.log({ newPeople })
+      this.setState(state =>({ people: newPeople }))
+      resolve(this.state.people)
+    })
+  }
+
+  updatePeople () {
+    this.state.people.forEach(person => {
+      person.move(this.sketch)
+      person.display(this.sketch)
+    })
+
+    window.requestAnimationFrame(this.updatePeople)
   }
 
   // ? Lifecycle
   componentDidMount () {
     this.storeParentSize()
+
+    setTimeout(() => {
+      this.sketch = new p5(Sketch)
+      console.log(this.state.width, this.state.height)
+      this.p5Setup()
+      // this.sketch.background(100)
+      this.createPeople()
+
+
+
+
+
+
+      // let updater = setInterval(() => {
+      //   this.sketch.background(100)
+      //   this.sketch.fill((Math.floor(Math.random() * 255)), 200, 200)
+      //   this.sketch.rect(200, 200, 50, 50)
+      // }, 32)
+
+    }, 500)
+
     // TODO Create people objects from contructor and stor in state
   }
 
   // ? Render
   render () {
-    return <div style={{
+    return <div id="parent" style={{
       position: 'absolute',
       top: 0,
       left: 0,
@@ -89,7 +169,7 @@ class Simulation extends React.Component {
       height: '100%',
     }} ref={ this.childRef } >
 
-      {
+      {/* {
         this.state.width && this.state.height && <Sketch
           style={{
             position: 'absolute',
@@ -126,13 +206,21 @@ class Simulation extends React.Component {
           }}
         />
 
-        }
+        } */}
     </div>
   }
 }
 
 
-Simulation.propTypes = {}
-Simulation.defaultProps = {}
+Simulation.propTypes = {
+  population: Number,
+  diameter: Number,
+  speed: Number,
+}
+Simulation.defaultProps = {
+  population: 50,
+  diameter: 10,
+  speed: 2,
+}
 
 export default Simulation
